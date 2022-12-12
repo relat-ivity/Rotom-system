5861cce8cd9765bf259b65d847d5680bb2c36bf9
3200105739 <3200105739@zju.edu.cn>
2022-12-12 18:57:41 +0800

    login finish

--- frontend/src/Component/Loginbox/Login.jsx
+++ frontend/src/Component/Loginbox/Login.jsx
@@ -3,18 +3,64 @@ import axios from 'axios'
 import './Login.css'
 import { Button, Checkbox, Form, Input } from 'antd';
 import { SetStateAction, useEffect, useState } from 'react';
-import { HashRouter as Router, Route, Navigate, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Navigate, Routes, useNavigate } from 'react-router-dom'
+import Password from 'antd/es/input/Password';
 
 const Login = () => {
-    return(
-        <div>
+    const navigate = useNavigate()
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div className='formbox'>
             <div className='loghead'>
-                <p>智能家居管理系统</p>
+                <img src="pic/lotomu.png" width={100}/> 
+                <p>Rotom智能家居管理系统</p>
             </div>
-            <div className='logform'>
+            <Form className='logform'
+                name="basic"
+                labelCol={{ span: 5 }}
+                wrapperCol={{ span: 16 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="用户名"
+                    name="username"
+                    rules={[{ required: true, message: '请输入用户名!' }]}
+                >
+                    <Input />
+                </Form.Item>
 
-            </div>
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[{ required: true, message: '请输入密码!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
 
+                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 5, span: 16 }}>
+                    <Checkbox>记住密码</Checkbox>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 5, span: 16 }}>
+                    <Button type="primary" htmlType="submit" >
+                        登录
+                    </Button>
+                    <Button type="link" htmlType="button" onClick={() => {navigate('regi')}}>
+                        注册
+                    </Button>
+                </Form.Item>
+            </Form>
+            <div className='clear'></div>
         </div>
     )
 }
--- frontend/src/Component/Regibox/Regi.jsx
+++ frontend/src/Component/Regibox/Regi.jsx
@@ -1,13 +1,72 @@
 import React from 'react'
 import axios from 'axios'
 import './Regi.css'
+import { Button, Checkbox, Form, Input } from 'antd';
 import { SetStateAction, useEffect, useState } from 'react';
-import { HashRouter as Router, Route, Navigate, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Navigate, Routes, useNavigate } from 'react-router-dom'
 
 const Regi = () => {
-    return(
+    const navigate = useNavigate()
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+
+    return (
         <div>
-            regi
+            <div className='formbox'>
+                <div className='loghead'>
+                    <img src="pic/lotomu.png" width={100} />
+                    <p>Rotom智能家居管理系统</p>
+                </div>
+                <Form className='regiform'
+                    name="basic"
+                    labelCol={{ span: 6 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="用户名"
+                        name="username"
+                        rules={[{ required: true, message: '请输入用户名!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="密码"
+                        name="password"
+                        rules={[{ required: true, message: '请输入密码!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="确认密码"
+                        name="password"
+                        rules={[{ required: true, message: '请输入密码!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+                        <Button type="primary" htmlType="submit" >
+                            注册
+                        </Button>
+                        <Button type="link" htmlType="button" onClick={() => { navigate('/login') }}>
+                            已拥有账号
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <div className='clear'></div>
+            </div>
         </div>
     )
 }
